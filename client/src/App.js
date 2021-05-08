f2a74ae79bf2128b0471e68e9f496a4b39dcdede
Love Fababeir <love.fababeir@hotmail.com>
2021-05-07 23:17:19 -0400

    started Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
 
 function App() {
-	return <div className="App">Wisejo</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route path="/" component={SignIn}></Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
