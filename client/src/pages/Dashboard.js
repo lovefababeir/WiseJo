b54f9edf9a4a2d94cfb4d8832587a67dc64aad50
Love Fababeir <love.fababeir@hotmail.com>
2021-05-08 00:03:39 -0400

    create base of the page to have signout button and user info

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { Alert } from "react-bootstrap";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const { currentUser, logout } = useAuth();
+	const { error, setError } = useState("");
+	const history = useHistory();
+	const signoutHandler = async e => {
+		e.preventDefault();
+		try {
+			await logout();
+			history.push("/");
+		} catch {
+			console.log("Sorry, we could not sign you out");
+			setError("Sorry, we could not sign you out");
+		}
+	};
+	return (
+		<div>
+			{error && <Alert variant="warning">{error}</Alert>}
+			<h1>{currentUser && currentUser.email}</h1>
+			<button onClick={signoutHandler}>Sign Out</button>
+		</div>
+	);
 };
 
 export default Dashboard;
