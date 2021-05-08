2e0b114f1d039536442366ef5702ae33795cbbf6
Love Fababeir <love.fababeir@hotmail.com>
2021-05-08 00:04:24 -0400

    added Firebase ui to the sign in page

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,7 +1,42 @@
+// Import FirebaseAuth and firebase.
 import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "../firebase";
+import { Link } from "react-router-dom";
+import { Card, Form, Alert } from "react-bootstrap";
 
-const SignIn = () => {
-	return <div>SignIn</div>;
+// Configure FirebaseUI.
+const uiConfig = {
+	// Popup signin flow rather than redirect flow.
+	signInFlow: "popup",
+	// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+	signInSuccessUrl: "/dashboard",
+	// We will display Google and Facebook as auth providers.
+	signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+		firebase.auth.GithubAuthProvider.PROVIDER_ID,
+		firebase.auth.EmailAuthProvider.PROVIDER_ID,
+		firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+	],
 };
 
-export default SignIn;
+function SignInScreen() {
+	return (
+		<div>
+			<Card>
+				<Card.Body>
+					<h1>My App</h1>
+					<p>Please sign-in:</p>
+					<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+					<div className="w-100 text-center mt-2">
+						Need an account? <Link to="/signup">Sign Up</Link>
+					</div>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+}
+
+export default SignInScreen;
