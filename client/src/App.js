bfbb2b705eb931e0cc7f11f65298a11a70701bf7
Love Fababeir <love.fababeir@hotmail.com>
2021-05-07 23:58:14 -0400

    implemented Auth context so that user info is available throughout app

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
-				<Switch>
-					<Route path="/" component={SignIn}></Route>
-					<Route path="/dashboard" component={Dashboard}></Route>
-				</Switch>
+				<AuthProvider>
+					<Switch>
+						<Route path="/" component={SignIn}></Route>
+						<Route path="/dashboard" component={Dashboard}></Route>
+					</Switch>
+				</AuthProvider>
 			</Router>
 		</div>
 	);
