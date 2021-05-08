9c9ee397c920531390f0afef479937001db950a3
Love Fababeir <love.fababeir@hotmail.com>
2021-05-07 23:41:00 -0400

    added Dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Router>
 				<Switch>
 					<Route path="/" component={SignIn}></Route>
+					<Route path="/dashboard" component={Dashboard}></Route>
 				</Switch>
 			</Router>
 		</div>
