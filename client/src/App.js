d6818de0dc8190bf8ac48ae7576543125dd838cf
Love Fababeir <love.fababeir@hotmail.com>
2021-05-08 01:28:20 -0400

    implemented private route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
 	return (
@@ -12,7 +13,7 @@ function App() {
 			<Router>
 				<AuthProvider>
 					<Switch>
-						<Route exact path="/" component={Dashboard}></Route>
+						<PrivateRoute exact path="/" component={Dashboard}></PrivateRoute>
 						<Route path="/login" component={SignIn}></Route>
 						<Route path="/signup" component={SignUp}></Route>
 						<Route path="/forgot-password" component={ForgotPassword}></Route>
