4fe4412bfe75d0ec6298042942c01baa0c234651
Love Fababeir <love.fababeir@hotmail.com>
2021-05-10 01:09:05 -0400

    component for loading spinner

---
+++ client/src/components/SearchFormSpinner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SearchFormSpinner.scss";
+
+const SearchFormSpinner = () => {
+	return (
+		<div className="loadingio-spinner-spinner-xix49am18ym">
+			<div className="ldio-cpn20gc8wlc">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default SearchFormSpinner;
