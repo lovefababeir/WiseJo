a02a594bc4b971d310348e12de42085f1bd7a182
Love Fababeir <love.fababeir@hotmail.com>
2021-05-09 23:28:16 -0400

    added contents of dashboard and links to other pages

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./Dashboard.scss";
+import jo from "../assets/images/WiseJo.png";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { Alert } from "react-bootstrap";
@@ -17,11 +19,45 @@ const Dashboard = () => {
 			setError("Sorry, we could not sign you out");
 		}
 	};
+
+	const pageHandler = page => {
+		history.push(`/${page}`);
+	};
 	return (
 		<div>
-			{error && <Alert variant="warning">{error}</Alert>}
-			<h1>{currentUser && currentUser.email}</h1>
-			<button onClick={signoutHandler}>Sign Out</button>
+			<section className="home">
+				<div className="home__logo-bg">
+					<img className="home__logo" src={jo} alt="Home logo" />
+				</div>
+				<h2 className="home__heading">
+					Hello Love!
+					<br />
+					What would you like to do today?
+				</h2>
+
+				<div className="home__nav">
+					<button className="home__btn" onClick={() => pageHandler("main")}>
+						Shop
+					</button>
+					<button className="home__btn" onClick={() => pageHandler("results")}>
+						Compare Prices
+					</button>
+					<button className="home__btn" onClick={() => pageHandler("receipt")}>
+						Log Shopping
+					</button>
+					<button
+						onClick={() => this.pageHandler("receiptslist")}
+						className="home__btn"
+					>
+						Track History
+					</button>
+				</div>
+				{error && <Alert variant="warning">{error}</Alert>}
+				<h1 className="home__userEmail">{currentUser && currentUser.email}</h1>
+				<button onClick={signoutHandler} className="home__signOut">
+					Sign Out
+				</button>
+			</section>
 		</div>
 	);
 };
