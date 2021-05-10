0ac8e238dcdcdb60f4adc213f3bb7a0fe884bb61
Love Fababeir <love.fababeir@hotmail.com>
2021-05-10 02:28:07 -0400

    rearrange routes and added menu

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
+import Search from "./pages/Search";
+import MenuBar from "./components/MenuBar";
 
 function App() {
 	return (
@@ -14,12 +16,17 @@ function App() {
 				<Router>
 					<AuthProvider>
 						<Switch>
-							<PrivateRoute exact path="/" component={Dashboard}></PrivateRoute>
-							<Route path="/login" component={SignIn}></Route>
-							<Route path="/signup" component={SignUp}></Route>
-							<Route path="/forgot-password" component={ForgotPassword}></Route>
+							<Route path="/login" component={SignIn} />
+							<Route path="/signup" component={SignUp} />
+							<Route path="/forgot-password" component={ForgotPassword} />
+							<PrivateRoute exact path="/" component={Dashboard} />
+							<PrivateRoute exact path="/shop" component={Search} />
 						</Switch>
 					</AuthProvider>
+					{(window.location.pathname === "/shop" ||
+						"/results" ||
+						"/record" ||
+						"/log") && <MenuBar />}
 				</Router>
 			</main>
 		</div>
