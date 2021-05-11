b5133eaed2eccda780a6464a065ab00473483477
Love Fababeir <love.fababeir@hotmail.com>
2021-05-11 13:30:57 -0400

    created endpoints for the receipts API requests which uses OCR

---
+++ server/routes/receipts.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const grocery = require("../webScrapingFunctions/groceryFunctions");
+const receiptsCollection = require("../data/receiptsCollection");
+const multer = require("multer");
+const fs = require("fs");
+var Tesseract = require("tesseract.js");
+
+var Storage = multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, "uploads/");
+	},
+	filename: (req, file, callback) => {
+		callback(null, Date.now() + ".jpeg");
+	},
+});
+
+const fileFilter = (req, file, cb) => {
+	if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
+const upload = multer({ storage: Storage, fileFilter: fileFilter });
+
+router.post("/upload", upload.single("receipt"), (req, res) => {
+	const time = Date.now();
+	const store = req.query.store.toLowerCase();
+	console.log("store", store);
+	var image = fs.readFileSync(req.file.path, {
+		encoding: null,
+	});
+
+	console.log("image", image);
+	Tesseract.recognize(image)
+		.then(result => {
+			const results = result.data.text.split(`\n`);
+
+			const purchaseData =
+				store === "walmart"
+					? grocery.walmartStore(results)
+					: store === "longo's"
+					? grocery.longosStore(results)
+					: grocery.nofrillsStore(results);
+
+			const receiptData = {
+				time: time,
+				store: store,
+				purchaseData: purchaseData,
+				results: result.data.text.split(`\n`),
+			};
+
+			receiptsCollection.push(receiptData);
+			res.status(200).json(receiptsCollection);
+		})
+		.catch(err => {
+			res
+				.status(400)
+				.send(
+					"Sorry could not read the receipt. Please try to take another picture with better focus and contrast.",
+					"Error",
+					err,
+				);
+		});
+});
+
+router.get("/history", (req, res) => {
+	console.log("made a request for history");
+	if (receiptsCollection) {
+		res.status(200).json(receiptsCollection);
+	} else {
+		res
+			.status(400)
+			.send(
+				"Sorry no data available. This mean that you have not yet added any receipts to your collection. Please add a receipt",
+			);
+	}
+});
+
+module.exports = router;
