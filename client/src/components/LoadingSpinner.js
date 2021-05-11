df41baf39679ea8e0e92691faf94b65f967dd29b
Love Fababeir <love.fababeir@hotmail.com>
2021-05-11 13:21:24 -0400

    placed spinner in another component so that it can be used in other pages

---
+++ client/src/components/LoadingSpinner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./LoadingSpinner.scss";
+
+const LoadingSpinner = () => {
+	return (
+		<div className="loadingio-spinner-spinner-xix49am18ym">
+			<div className="ldio-cpn20gc8wlc">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingSpinner;
