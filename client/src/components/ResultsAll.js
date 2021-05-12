3605d02c8a9e4084c844d8dac160723d4ff007c6
Love Fababeir <love.fababeir@hotmail.com>
2021-05-12 14:29:13 -0400

    added keys and fixed the use of react-spring library which has been upated

--- client/src/components/ResultsAll.js
+++ client/src/components/ResultsAll.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./ResultsAll.scss";
 import deleteIcon from "../assets/images/delete.svg";
-// import { Spring } from "react-spring";
+import { useSpring, animated } from "react-spring";
+import { v4 as uuidv4 } from "uuid";
 
 const ResultsAll = ({ list, deleteHandler }) => {
 	var listOfStores = [];
@@ -22,62 +23,61 @@ const ResultsAll = ({ list, deleteHandler }) => {
 		});
 	};
 	storeList();
+
+	const details = useSpring({
+		from: { opacity: 0, marginTop: -500 },
+		to: { opacity: 1, marginTop: 0 },
+		config: { delay: 1000, duration: 2000 },
+	});
 	return (
-		// <Spring
-		// 	from={{ opacity: 0, marginTop: -500 }}
-		// 	to={{ opacity: 1, marginTop: 0 }}
-		// 	config={{ delay: 1000, duration: 2000 }}
-		// >
-		// 	{props => (
-		// 		<div style={props}>
-		<section className="store">
-			{listOfStores.map((store, i) => {
-				return (
-					<>
-						<div
-							className={`store__logo store__logo--${
-								store.store === "Walmart" ? "0" : store.store === "Longo's" ? "1" : "2"
-							}`}
-						></div>
-						<div className={`store__box store__box--${store.store}`}>
-							{store.listOfItems
-								.sort((a, b) => {
-									return a.value * a.quantity - b.value * b.quantity;
-								})
-								.map(item => {
-									return (
-										<div className="item">
-											<img
-												className="item__image"
-												src={item.image}
-												alt={`The item with capacity: (${item.capacity}) at ${item.store}`}
-											/>
-											<p className="item__title">{item.title}</p>
-											<div className="item__details">
-												<div>
-													<p className="item__price">{item.price}</p>
-													<p className="item__capacity">{item.capacity}</p>
-												</div>
+		<animated.div style={details}>
+			<section className="store">
+				{listOfStores.map((store, i) => {
+					return (
+						<div key={uuidv4()}>
+							<div
+								className={`store__logo store__logo--${
+									store.store === "Walmart" ? "0" : store.store === "Longo's" ? "1" : "2"
+								}`}
+							></div>
+							<div className={`store__box store__box--${store.store}`}>
+								{store.listOfItems
+									.sort((a, b) => {
+										return a.value * a.quantity - b.value * b.quantity;
+									})
+									.map(item => {
+										return (
+											<div className="item" key={uuidv4()}>
 												<img
-													src={deleteIcon}
-													alt="Click here to delete item"
-													onClick={e => {
-														deleteHandler("item", item.productID);
-													}}
-													className="item__delete"
+													className="item__image"
+													src={item.image}
+													alt={`The item with capacity: (${item.capacity}) at ${item.store}`}
 												/>
+												<p className="item__title">{item.title}</p>
+												<div className="item__details">
+													<div>
+														<p className="item__price">{item.price}</p>
+														<p className="item__capacity">{item.capacity}</p>
+													</div>
+													<img
+														src={deleteIcon}
+														alt="Click here to delete item"
+														onClick={e => {
+															deleteHandler("item", item.productID);
+														}}
+														className="item__delete"
+													/>
+												</div>
 											</div>
-										</div>
-									);
-								})}
+										);
+									})}
+							</div>
 						</div>
-					</>
-				);
-			})}
-		</section>
-		// 		</div>
-		// 	)}
-		// </Spring>
+					);
+				})}
+			</section>
+			//{" "}
+		</animated.div>
 	);
 };
 
