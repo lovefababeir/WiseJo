1aff5bf25b1f40f05afbce2776a821c86bb9af01
Love Fababeir <love.fababeir@hotmail.com>
2021-05-13 14:57:37 -0400

    moved functions for longos to its own js sheet

---
+++ server/webScrapingFunctions/gateway.js
@@ -0,0 +1,135 @@
+const puppeteer = require("puppeteer");
+
+const store = async function (searchWords) {
+	const options = { headless: true };
+	var browser = await puppeteer.launch(options);
+	var page = await browser.newPage();
+	await page.setDefaultTimeout(120000);
+	await page.setViewport({ height: 1200, width: 960 });
+	await page.goto(
+		`https://www.grocerygateway.com/store/groceryGateway/en/search/?text=${searchWords}`,
+		{
+			waitUntil: "networkidle2",
+		},
+	);
+
+	// const searchGateway = "#js-site-search-input";
+	// await page.type(searchGateway, searchWords, {
+	// 	delay: 100,
+	// });
+	// await page.keyboard.press("Enter");
+	// await page.reload();
+
+	let result = await page.evaluate(async () => {
+		let topResults = [];
+
+		for (var i = 1; i < 7; i++) {
+			const productID = document
+				.querySelector(
+					`ul[class="products-gallery js-quickview-wrapper"]> :nth-child(${i})`,
+				)
+				.getAttribute("data-sku");
+			const image = document
+				.querySelector(
+					`ul[class="products-gallery js-quickview-wrapper"]> :nth-child(${i}) img`,
+				)
+				.getAttribute("src");
+
+			const title = document.querySelector(
+				`ul[class="products-gallery js-quickview-wrapper"]> :nth-child(${i}) a[class="product-card__name ellipsis"]>strong`,
+			).innerText;
+			const price = document.querySelector(
+				`ul[class="products-gallery js-quickview-wrapper"]> :nth-child(${i}) span[class="cart_reader"]`,
+			).innerText;
+
+			const m = document.querySelector(
+				`ul[class="products-gallery js-quickview-wrapper"] > li:nth-child(${i}) > div > div > div.product-card__bottom-content > span.product-card__price + span`,
+			).innerText;
+
+			const capacity = m.substr(m.indexOf("/") + 2);
+
+			//function to get the capacity of each item as a number
+			const val = C => {
+				var capacity = C.toLowerCase();
+				var value;
+
+				if (capacity.includes("x")) {
+					capacity = capacity.split("x").find(str => {
+						return str.toLowerCase().includes("l") || str.toLowerCase().includes("g");
+					});
+				}
+
+				if (capacity.includes("kg")) {
+					value = capacity.match(/\d+/g).map(Number)[0] * 1000;
+				} else if (capacity.includes("g") || capacity.includes("ml")) {
+					value = capacity.match(/\d+/g).map(Number)[0];
+					return value;
+				} else if (capacity.includes("l")) {
+					value = capacity.match(/\d+/g).map(Number)[0] * 1000;
+				} else if (
+					capacity.includes("pkg ") ||
+					capacity.includes("pack") ||
+					capacity.includes("cup") ||
+					capacity.includes("cans") ||
+					capacity.includes("count") ||
+					capacity.includes("ea")
+				) {
+					value = 1;
+				} else {
+					value = 1;
+				}
+				return value;
+			};
+			//function to get the quantity in item
+			const qty = C => {
+				var capacity = C.toLowerCase();
+				var qty;
+				if (capacity.includes("x")) {
+					capacity = capacity.split("x").find(str => {
+						return (
+							!str.toLowerCase().includes("l") && !str.toLowerCase().includes("g")
+						);
+					});
+					return capacity.match(/\d+/g).map(Number)[0];
+				}
+				if (
+					capacity.includes("pkg") ||
+					capacity.includes("pack") ||
+					capacity.includes("cup") ||
+					capacity.includes("cans") ||
+					capacity.includes("count") ||
+					capacity.includes("ea")
+				) {
+					qty = capacity.match(/\d+/g).map(Number)[0];
+				} else {
+					qty = 1;
+				}
+				return qty;
+			};
+
+			const quantity = qty(capacity);
+
+			const value = val(capacity);
+
+			topResults.push({
+				store: "Longo's",
+				productID: productID,
+				image: image,
+				title: title,
+				price: price,
+				capacity: capacity,
+				value: value,
+				quantity: quantity,
+			});
+		}
+
+		return topResults;
+	});
+
+	return result;
+	await page.waitFor(2000);
+	await page.close();
+	await browser.close();
+};
+
+module.exports = { store };
