4bca7e5aa2507a8167a748cf4979910f8bd8c492
Love Fababeir <love.fababeir@hotmail.com>
2021-05-15 01:18:44 -0400

    created compoenent for the sub menu items in the results page and moved the respective code

--- client/src/pages/ResultsSubMenu.js
+++ client/src/pages/ResultsSubMenu.js
@@ -1,40 +1,29 @@
 import React from "react";
 import scale from "../assets/images/scale.svg";
 import store from "../assets/images/store.svg";
-import sun from "../assets/images/WiseJo.png";
+import wisejo from "../assets/images/WiseJo.png";
+import ResultsSubMenuItem from "../components/ResultsSubMenuItem";
+import { v4 as uuidv4 } from "uuid";
 
 const ResultsSubMenu = ({ changePageHandler, view }) => {
+	const subMenuItems = [
+		{ subpage: "All", icon: store, altMessage: "View results by store" },
+		{ subpage: "Capacity", icon: scale, altMessage: "View results by capacity" },
+		{ subpage: "Solutions", icon: wisejo, altMessage: "view WiseJo's advice" },
+	];
+
 	return (
 		<nav className="results__nav">
 			<ul className="results__menu">
-				<li
-					className={`${
-						view === "All" ? "results__menu-item--selected" : "results__menu-item"
-					}`}
-					onClick={() => changePageHandler("All")}
-				>
-					<img src={store} className="results__store" alt="view by store" />
-				</li>
-				<li
-					className={`${
-						view === "Capacity"
-							? "results__menu-item--selected"
-							: "results__menu-item"
-					}`}
-					onClick={() => changePageHandler("Capacity")}
-				>
-					<img src={scale} className="results__store" alt="view by capacity" />
-				</li>
-				<li
-					className={`${
-						view === "Solutions"
-							? "results__menu-item--selected"
-							: "results__menu-item"
-					}`}
-					onClick={() => changePageHandler("Solutions")}
-				>
-					<img src={sun} className="results__store" alt="view WiseJo's advice" />
-				</li>
+				{subMenuItems.map(item => (
+					<ResultsSubMenuItem
+						view={view}
+						subpage={item.subpage}
+						changePageHandler={changePageHandler}
+						icon={item.icon}
+						key={uuidv4()}
+					/>
+				))}
 			</ul>
 		</nav>
 	);
