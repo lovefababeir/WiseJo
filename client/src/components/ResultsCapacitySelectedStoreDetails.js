7b90f471797cf29c7ec83017cf21a51d7e506ead
Love Fababeir <love.fababeir@hotmail.com>
2021-05-17 21:57:21 -0400

    wrapped text in p elements to make it easier to give them styles

--- client/src/components/ResultsCapacitySelectedStoreDetails.js
+++ client/src/components/ResultsCapacitySelectedStoreDetails.js
@@ -8,10 +8,13 @@ const ResultsCapacitySelectedStoreDetails = ({
 }) => {
 	return (
 		<>
-			<p className="capacity__options">
-				<em>{store}</em> - {price}
-				{quantity > 1 ? " for pack of " + quantity : null} ( {title} )
-			</p>
+			<div className="capacity__options">
+				<p>{store} </p> -
+				<p>
+					{price}
+					{quantity > 1 ? " for pack of " + quantity : null} ( {title} )
+				</p>
+			</div>
 		</>
 	);
 };
