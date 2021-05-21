759f0b00ea65a09d828f6b445b85648f5fe57165
Love Fababeir <love.fababeir@hotmail.com>
2021-05-21 18:19:50 -0400

    Created new route that decodes the results from the OCR API request which is done on client side

--- server/routes/receipts.js
+++ server/routes/receipts.js
@@ -1,15 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const grocery = require("../receiptFunctions/groceryFunctions");
+const walmart = require("../receiptFunctions/walmartReceipt");
 const receiptsCollection = require("../data/receiptsCollection");
 const multer = require("multer");
 const fs = require("fs");
-var Tesseract = require("tesseract.js");
-const vision = require("@google-cloud/vision");
-const client = new vision.ImageAnnotatorClient({
-	keyFilename: "wisejo-ea0d54e05e6c.json",
+const axios = require("axios").default;
+require("dotenv").config();
+const fcn = require("../receiptFunctions/decodeText");
+
+router.post("/convertImage", (req, res) => {
+	const store = req.query.store.toLowerCase();
+	const time = parseInt(req.query.time);
+	const convertedText = fcn.decodeText(req.body.data.regions);
+	let purchaseData;
+	if (store === "walmart") {
+		purchaseData = walmart.receipt(convertedText);
+	} else if (store === "longo's") {
+		purchaseData =
+			"Sorry, algorithm for reading Longos receipts is currently not working";
+	} else if (store === "no frills") {
+		purchaseData =
+			"Sorry, algorithm for reading No Frills receipts is currently not working";
+	} else {
+		purchaseData = `Sorry, algorithm for reading ${store} receipts is currently not working`;
+	}
+
+	const receiptData = {
+		time: time,
+		store: store,
+		purchaseData: purchaseData,
+		results: convertedText,
+	};
+
+	console.log("receiptData", receiptData, "purchase data:", purchaseData);
+	receiptsCollection.push(receiptData);
+	res.status(200).json(receiptsCollection);
 });
 
+router.get("/history", (req, res) => {
+	console.log("made a request for history");
+	if (receiptsCollection) {
+		res.status(200).json(receiptsCollection);
+	} else {
+		res
+			.status(400)
+			.send(
+				"Sorry no data available. This mean that you have not yet added any receipts to your collection. Please add a receipt",
+			);
+	}
+});
+
+//====================================
+//receipt OCR API for when storing the image
 var Storage = multer.diskStorage({
 	destination: function (req, file, callback) {
 		callback(null, "uploads/");
@@ -34,32 +76,32 @@ router.post("/upload", upload.single("receipt"), (req, res) => {
 	console.log(name);
 	const store = req.query.store.toLowerCase();
 	console.log("store", store);
-	// const fileName = "/uploads/1621555926078.jpg";
 	var image = fs.readFileSync(req.file.path, {
 		encoding: null,
 	});
 
-	async function readText(pic) {
-		const [result] = await client.textDetection(pic);
-		const detections = result.textAnnotations;
-		console.log("Text:");
-		// detections.forEach(text => console.log(text));
-		console.log(detections[0].description);
-	}
-	readText(image);
-});
-
-router.get("/history", (req, res) => {
-	console.log("made a request for history");
-	if (receiptsCollection) {
-		res.status(200).json(receiptsCollection);
-	} else {
-		res
-			.status(400)
-			.send(
-				"Sorry no data available. This mean that you have not yet added any receipts to your collection. Please add a receipt",
-			);
-	}
+	//=============
+	axios
+		.post(`${process.env.OCR_ENDPOINT}vision/v3.2/ocr`, req.file.image, {
+			headers: {
+				"content-type": "application/json",
+				"Ocp-Apim-Subscription-Key": process.env.OCR_API_KEY_1,
+			},
+		})
+		.then(res => {
+			console.log(res);
+		})
+		.catch(err => console.log(err.response));
+	//=================================
+	//VISION API
+	// async function readText(pic) {
+	// 	const [result] = await client.textDetection(pic);
+	// 	const detections = result.textAnnotations;
+	// 	console.log("Text:");
+	// 	// detections.forEach(text => console.log(text));
+	// 	console.log(detections[0].description);
+	// }
+	// readText(image);
 });
 
 module.exports = router;
