ca5f3f52cc3c37f66ac13fc60340da6402b031c5
Love Fababeir <love.fababeir@hotmail.com>
2021-05-21 18:22:37 -0400

    removed code from general grocery functions js and placed it in its own js

---
+++ server/receiptFunctions/walmartReceipt.js
@@ -0,0 +1,53 @@
+const receipt = receiptResults => {
+	//To get store ID
+	const storeIDindex = receiptResults.findIndex(text => {
+		return text.includes("STORE");
+	});
+	const storeID = receiptResults[storeIDindex].split(" ")[1];
+
+	//To get address
+	const address =
+		receiptResults[storeIDindex + 1] +
+		" " +
+		receiptResults[storeIDindex + 2] +
+		" " +
+		receiptResults[storeIDindex + 3];
+
+	//To index of where List of items starts
+	const afterAddress = receiptResults.slice(storeIDindex + 4);
+	const itemsIndex =
+		afterAddress.findIndex(text => {
+			return text.includes("ST");
+		}) + 1;
+
+	//SUBTOTAL
+	const subtotalIndex = afterAddress.findIndex(text => {
+		return text.includes("SUBTOTAL") || text.includes("SUB");
+	});
+	const subtotalStr = afterAddress[subtotalIndex];
+	const subtotal = Number(subtotalStr.replace(/[^0-9.-]+/g, ""));
+
+	//LIST OF ITEMS PURCHASED
+	const purchases = afterAddress.slice(itemsIndex, subtotalIndex);
+
+	//TOTAL
+	const withTotal = afterAddress.slice(subtotalIndex + 1);
+	const totalIndex = withTotal.findIndex(text => {
+		return text.includes("TOTAL") || text.includes("TAL");
+	});
+
+	// const totalStr = withTotal[totalIndex];
+	const total = Number(withTotal[totalIndex].replace(/[^0-9.-]+/g, ""));
+
+	const storeData = {
+		storeID: storeID,
+		address: address,
+		purchases: purchases,
+		subtotal: subtotal,
+		total: total,
+	};
+
+	return storeData;
+};
+
+module.exports = { receipt };
