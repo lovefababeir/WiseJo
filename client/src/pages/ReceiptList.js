207f535c2d67e05f3996dc3b9319abaef880f280
Love Fababeir <love.fababeir@hotmail.com>
2021-05-23 00:53:55 -0400

    Coverted component to a functional component using React hooks

--- client/src/pages/ReceiptList.js
+++ client/src/pages/ReceiptList.js
@@ -1,26 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./ReceiptList.scss";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 
-class ReceiptsList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			receiptsList: "",
-			receiptSelected: "",
-		};
-	}
-	componentDidMount() {
+const ReceiptList = () => {
+	const [receiptList, setReceiptList] = useState("");
+	const [receiptSelected, setReceiptSelected] = useState("");
+
+	useEffect(() => {
+		let mounted = true;
+		console.log("mounted");
 		axios
 			.get(`${process.env.REACT_APP_BASE_URL}receipts/history`)
 			.then(result => {
-				this.setState({ receiptsList: result.data });
+				if (mounted) {
+					console.log(result);
+					setReceiptList(result.data);
+					console.log("receiptList:", result.data);
+				}
 			})
 			.catch(err => console.log("Could not get list", err));
-	}
+		return () => {
+			mounted = false;
+		};
+	}, []);
 
-	convertDate = timestamp => {
+	const convertDate = timestamp => {
 		let currentTime = Date.now();
 		let timeLapsed = currentTime - timestamp;
 		let dateSubmitted = new Date(timestamp);
@@ -46,18 +51,18 @@ class ReceiptsList extends Component {
 		}
 	};
 
-	selectReceiptHandler = (e, ID) => {
+	const selectReceiptHandler = (e, ID) => {
 		e.preventDefault();
 		// const selectedItemID = e.target.value;
 
-		const receiptSelected = this.state.receiptsList.find(item => {
+		const receiptSelected = receiptList.find(item => {
 			return item.time === ID;
 		});
 
-		this.setState({ receiptSelected: receiptSelected });
+		setReceiptSelected(receiptSelected);
 	};
 
-	totalExpenses = (list, duration) => {
+	const totalExpenses = (list, duration) => {
 		return list.reduce((total, receipt) => {
 			//first is day in ms second is week in ms
 			const timeDiff = duration === "day" ? 86400000 : 604800000;
@@ -76,89 +81,83 @@ class ReceiptsList extends Component {
 		}, 0);
 	};
 
-	render() {
-		return (
-			<>
-				<div className="receiptsList">
-					<h1 className="results__title results__title--receiptsList">
-						SNAP & TRACK
-					</h1>
-					<div className="receiptsList__box">
-						<h2 className="receiptsList__title">Record of Expenses</h2>
-						<div className="receipt__record table-headings">
-							<p className="receipt__summary">Store:</p>
+	return (
+		<>
+			<div className="receiptsList">
+				<h1 className="results__title results__title--receiptList">SNAP & TRACK</h1>
+				<div className="receiptsList__box">
+					<h2 className="receiptsList__title">Record of Expenses</h2>
+					<div className="receipt__record table-headings">
+						<p className="receipt__summary">Store:</p>
 
-							<p className="receipt__summary">Date Logged:</p>
-							<p className="receipt__summary">Total:</p>
+						<p className="receipt__summary">Date Logged:</p>
+						<p className="receipt__summary">Total:</p>
+					</div>
+					{receiptList &&
+						receiptList.map(receipt => {
+							return (
+								<div
+									className="receipt__record"
+									id={receipt.time}
+									key={receipt.time}
+									onClick={e => selectReceiptHandler(e, receipt.time)}
+								>
+									<p className="receipt__summary">{receipt.store}</p>
+
+									<p className="receipt__summary">{convertDate(receipt.time)}</p>
+									<p className="receipt__summary">${receipt.purchaseData.total}</p>
+								</div>
+							);
+						})}
+					<div className="receipt__record">
+						<h3 className="receiptsList__dayTotal">
+							Today you spent:{" "}
+							<span className="receiptsList__dayTotal--Num">
+								{receiptList &&
+									`$
+									${totalExpenses(receiptList, "day").toFixed(2)}`}
+							</span>
+						</h3>
+						<h3 className="receiptsList__weekTotal">
+							This week you spent:{" "}
+							<span className="receiptsList__weekTotal--Num">
+								{receiptList &&
+									`$
+									${totalExpenses(receiptList, "week").toFixed(2)}`}
+							</span>
+						</h3>
+					</div>
+				</div>
+				{receiptSelected && (
+					<div className="receipts-selected">
+						<div className="receipts-selected__storeDetails">
+							<p className="receipts-selected__store">{receiptSelected.store}</p>
+							<p className="receipts-selected__receipt">
+								Store ID:
+								{receiptSelected.purchaseData.storeID}
+							</p>
+							<p className="receipts-selected__receipt">
+								{receiptSelected.purchaseData.address}
+							</p>
 						</div>
-						{this.state.receiptsList &&
-							this.state.receiptsList.map(receipt => {
+						<div className="receipts-selected__itemsList">
+							<p className="receipts-selected__item-title">Items Purchased:</p>
+							{receiptSelected.purchaseData.purchases.map(item => {
 								return (
-									<div
-										className="receipt__record"
-										id={receipt.time}
-										key={receipt.time}
-										onClick={e => this.selectReceiptHandler(e, receipt.time)}
-									>
-										<p className="receipt__summary">{receipt.store}</p>
-
-										<p className="receipt__summary">{this.convertDate(receipt.time)}</p>
-										<p className="receipt__summary">${receipt.purchaseData.total}</p>
-									</div>
+									<p className="receipts-selected__item" key={uuidv4()}>
+										{item}
+									</p>
 								);
 							})}
-						<div className="receipt__record">
-							<h3 className="receiptsList__dayTotal">
-								Today you spent:{" "}
-								<span className="receiptsList__dayTotal--Num">
-									{this.state.receiptsList &&
-										`$
-									${this.totalExpenses(this.state.receiptsList, "day").toFixed(2)}`}
-								</span>
-							</h3>
-							<h3 className="receiptsList__weekTotal">
-								This week you spent:{" "}
-								<span className="receiptsList__weekTotal--Num">
-									{this.state.receiptsList &&
-										`$
-									${this.totalExpenses(this.state.receiptsList, "week").toFixed(2)}`}
-								</span>
-							</h3>
+							<p className="receipts-selected__total">
+								Total Spent: ${receiptSelected.purchaseData.total}
+							</p>
 						</div>
 					</div>
-					{this.state.receiptSelected && (
-						<div className="receipts-selected">
-							<div className="receipts-selected__storeDetails">
-								<p className="receipts-selected__store">
-									{this.state.receiptSelected.store}
-								</p>
-								<p className="receipts-selected__receipt">
-									Store ID:
-									{this.state.receiptSelected.purchaseData.storeID}
-								</p>
-								<p className="receipts-selected__receipt">
-									{this.state.receiptSelected.purchaseData.address}
-								</p>
-							</div>
-							<div className="receipts-selected__itemsList">
-								<p className="receipts-selected__item-title">Items Purchased:</p>
-								{this.state.receiptSelected.purchaseData.purchases.map(item => {
-									return (
-										<p className="receipts-selected__item" key={uuidv4()}>
-											{item}
-										</p>
-									);
-								})}
-								<p className="receipts-selected__total">
-									Total Spent: ${this.state.receiptSelected.purchaseData.total}
-								</p>
-							</div>
-						</div>
-					)}
-				</div>
-			</>
-		);
-	}
-}
+				)}
+			</div>
+		</>
+	);
+};
 
-export default ReceiptsList;
+export default ReceiptList;
