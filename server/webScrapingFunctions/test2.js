e8333701c101949cb7a6534d0e9ae7c25ab66673
Love Fababeir <love.fababeir@hotmail.com>
2021-05-22 20:55:14 -0400

    added function for the longos receipt

--- server/webScrapingFunctions/test2.js
+++ server/webScrapingFunctions/test2.js
@@ -1,3 +1,5 @@
+const { text } = require("express");
+
 const receiptResults = [
 	"STORE 1080",
 	"5995 STEELES AVENUE ERST",
@@ -16,7 +18,7 @@ const receiptResults = [
 	"CHANGE DUE",
 ];
 
-const receipt = receiptResults => {
+const walmartreceipt = receiptResults => {
 	//To get store ID
 	const storeIDindex = receiptResults.findIndex(text => {
 		return text.includes("STORE");
@@ -56,7 +58,7 @@ const receipt = receiptResults => {
 
 	//LIST OF ITEMS PURCHASED
 	const purchases = afterAddress.slice(itemsIndex, subtotalIndex);
-	// console.log(purchases);
+
 	//TOTAL
 	const withTotal = afterAddress.slice(subtotalIndex + 1);
 	const totalIndex = withTotal.findIndex(text => {
@@ -576,4 +578,123 @@ const decodeText = result => {
 	return organized(sorted);
 };
 
-decodeText(resultLongos);
+const longosReceipt = decodeText(resultLongos);
+
+const longosReceiptData = receiptResults => {
+	//To get store details ID
+	const storeDetailsIndex = receiptResults.findIndex(text => {
+		return text.includes("Cashier");
+	});
+
+	//store details
+	const storeDetails = receiptResults.splice(0, storeDetailsIndex + 1);
+
+	let contact;
+	let storeLocation;
+	let manager;
+	let cashier;
+	//=====Store location and contact details:
+	if (storeDetails) {
+		const locationDetails = storeDetails
+			? storeDetails
+					.find(line => {
+						return line.includes("Longos") && line.includes("-");
+					})
+					.split(" ")
+			: [];
+
+		if (locationDetails.length) {
+			const contactIndex = locationDetails.findIndex(word => {
+				return word.includes("-");
+			});
+			contact = locationDetails.splice(contactIndex)[0];
+			storeLocation = locationDetails.join(" ");
+		} else {
+			contact = "No info detected";
+			storeLocation = "No info detected";
+		}
+		//store Manager:
+		const lineWithManager =
+			storeDetails
+				.find(line => {
+					return line.includes("Manager");
+				})
+				.split(" ") || "";
+		manager = lineWithManager
+			.slice(
+				lineWithManager.findIndex(word => {
+					return word.includes("is");
+				}) + 1,
+			)
+			.join(" ");
+
+		//Cashier details:
+		const cashierLine = storeDetails
+			.find(line => {
+				return line.includes("Cashier");
+			})
+			.split(" ");
+
+		const cashierIndex = cashierLine.findIndex(word => {
+			return word.includes("was");
+		});
+		cashier = cashierLine[cashierIndex + 1];
+	}
+
+	//To find the index of where List of items starts
+	const firstItemsIndex =
+		receiptResults.findIndex(line => {
+			return line.includes("$");
+		}) - 1;
+	const lastItemIndex = receiptResults.findIndex(line => {
+		return (
+			(line.includes("Items") && line.includes("Subtotal")) ||
+			(line.includes("$") && line.includes("Items"))
+		);
+	});
+	let itemsList;
+	if (firstItemsIndex + 1 > 0 && lastItemIndex + 1 > 0) {
+		itemsList = receiptResults.slice(firstItemsIndex, lastItemIndex);
+	}
+
+	//SUBTOTAL
+	let subtotal;
+	const subtotalLine = receiptResults
+		.find(text => {
+			return text.includes("Subtotal");
+		})
+		.split(" ");
+
+	if (subtotalLine.length > 0) {
+		const subtotalStr = subtotalLine.pop();
+		subtotal = Number(subtotalStr.replace(/[^0-9.-]+/g, ""));
+	}
+
+	//TOTAL
+	let total;
+	const totalLine = receiptResults
+		.find(line => {
+			return line.includes("TOTAL") || line.includes("Item count");
+		})
+		.split(" ");
+	if (totalLine) {
+		const totalStr = totalLine.pop();
+
+		// const totalStr = withTotal[totalIndex];
+		total = Number(totalStr.replace(/[^0-9.-]+/g, ""));
+	}
+
+	const storeData = {
+		storeID: storeLocation || "No info detected",
+		manager: manager || "No info detected",
+		cashier: cashier || "No info detected",
+		contact: contact || "No info detected",
+		purchases: itemsList || "No info detected",
+		subtotal: subtotal || "No info detected",
+		total: total || "No info detected",
+	};
+
+	return storeData;
+};
+
+console.log(longosReceiptData(longosReceipt));
