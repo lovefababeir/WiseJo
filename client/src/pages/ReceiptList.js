5d4b34f251bff53605aa7f4446a3e86f1eae064b
Love Fababeir <love.fababeir@hotmail.com>
2021-05-22 23:50:56 -0400

    Stored base url in a variable that is retrieved from the env.

--- client/src/pages/ReceiptList.js
+++ client/src/pages/ReceiptList.js
@@ -13,7 +13,7 @@ class ReceiptsList extends Component {
 	}
 	componentDidMount() {
 		axios
-			.get("http://localhost:5000/receipts/history")
+			.get(`${process.env.REACT_APP_BASE_URL}receipts/history`)
 			.then(result => {
 				this.setState({ receiptsList: result.data });
 			})
