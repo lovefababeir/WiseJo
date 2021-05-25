ee5efd725549513ccfb6ea079bb6aa5dd91be2c9
Love Fababeir <love.fababeir@hotmail.com>
2021-05-24 23:41:04 -0400

    Fixed issue of items being in the same line number.  Removed the use of line numbers and assessed every line based on their vertical distance which is postion[1] from the next line to determine if the should appear in the same line.

--- server/receiptFunctions/decodeText.js
+++ server/receiptFunctions/decodeText.js
@@ -27,57 +27,42 @@ const decodeText = result => {
 	const organized = list => {
 		const lastItem = list.length - 1;
 		const lastItemPositionY = Math.floor(list[lastItem].position[1] / 100);
-		const heightY = lastItemPositionY > 15 ? 30 : 50;
+
+		const heightY = 25;
 
 		//FINAL ORDERED LIST
 		const orderedList = [];
-
-		//Check through all rows in the sorted and assess which are ready to add
-		for (let i = 0; i < lastItemPositionY + 1; i++) {
-			//list of items with the same Y/row number
-			const ithRow = list.filter(item => {
-				return Math.floor(item.position[1] / 100) === i;
-			});
-
-			//if there is just 1 item then just add it to the orderedList
-			if (ithRow.length === 1) {
-				orderedList.push(ithRow[0].line);
-			} else if (ithRow.length > 1) {
-				//index of the item that is the first of line 2
-				const indexLine2 = ithRow.findIndex((item, k, ithRow) => {
-					if (ithRow[k - 1]) {
-						return ithRow[k].position[1] - ithRow[k - 1].position[1] > heightY;
+		while (list.length > 0) {
+			let newLine;
+			let indexNextLine;
+			if (list.length === 1) {
+				newLine = list.shift();
+				orderedList.push(newLine.line);
+			} else {
+				indexNextLine = list.findIndex((item, k, list) => {
+					if (list[k - 1]) {
+						return list[k].position[1] - list[k - 1].position[1] >= heightY;
+					} else {
+						return false;
 					}
-					return false;
 				});
-
-				if (indexLine2 + 1 > 0) {
-					const line1 = ithRow.slice(0, indexLine2).sort((a, b) => {
+				if (indexNextLine + 1 > 0) {
+					const nextLine = list.splice(0, indexNextLine).sort((a, b) => {
 						return a.position[0] - b.position[0];
 					});
-					const line2 = ithRow.slice(indexLine2).sort((a, b) => {
-						return a.position[0] - b.position[0];
-					});
-
 					orderedList.push(
-						line1
-							.map(item => {
-								return item.line;
-							})
-							.join("  "),
-						line2
+						nextLine
 							.map(item => {
 								return item.line;
 							})
 							.join("  "),
 					);
 				} else {
-					const line1 = ithRow.sort((a, b) => {
+					const nextLine = list.splice(0, list.length).sort((a, b) => {
 						return a.position[0] - b.position[0];
 					});
-
 					orderedList.push(
-						line1
+						nextLine
 							.map(item => {
 								return item.line;
 							})
@@ -86,9 +71,9 @@ const decodeText = result => {
 				}
 			}
 		}
+
 		return orderedList;
 	};
-	console.log(organized(sorted));
 	return organized(sorted);
 };
 
