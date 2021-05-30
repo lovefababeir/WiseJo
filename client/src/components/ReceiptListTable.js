e88e2672959619071ad81002b378f5e0a5bc83e1
Love Fababeir <love.fababeir@hotmail.com>
2021-05-29 23:46:34 -0400

    Created component for the table for the receipts.

---
+++ client/src/components/ReceiptListTable.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReceiptListTableElements from "./ReceiptListTableElements";
+import ReceiptListTableSummary from "./ReceiptListTableSummary";
+
+const ReceiptListTable = ({
+	receiptList,
+	selectReceiptHandler,
+	totalExpenses,
+}) => {
+	return (
+		<>
+			<div className="receiptsList__box">
+				<h2 className="receiptsList__title">Record of Expenses</h2>
+				<div className="receipt__record table-headings">
+					<p className="receipt__summary">Store:</p>
+					<p className="receipt__summary">Date Logged:</p>
+					<p className="receipt__summary">Total:</p>
+				</div>
+				{receiptList &&
+					receiptList.map(receipt => {
+						return (
+							<ReceiptListTableElements
+								receipt={receipt}
+								selectReceiptHandler={selectReceiptHandler}
+							/>
+						);
+					})}
+				<ReceiptListTableSummary
+					receiptList={receiptList}
+					totalExpenses={totalExpenses}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default ReceiptListTable;
