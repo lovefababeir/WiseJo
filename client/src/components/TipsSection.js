9c49cb59de81ae7b6775bef8080cbc2cff9487cb
Love Fababeir <love.fababeir@hotmail.com>
2021-05-31 15:47:04 -0400

    added more tips for better search results.

--- client/src/components/TipsSection.js
+++ client/src/components/TipsSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./TipsSection.scss";
 
 const TipsSection = ({ page }) => {
+	console.log(page);
 	return (
 		<section className="tips">
 			<h1 className="tips__title">Not the results you want? Here are some tips</h1>
@@ -12,11 +13,17 @@ const TipsSection = ({ page }) => {
 					to this page to see what new adice I have for you.
 				</p>
 			)}
+			{page === "all" && (
+				<p className="tips__tip">
+					The results you see are the top 6 returned from each store according the
+					item you submitted in the Shop form. Try changing your search to something
+					more specific.
+				</p>
+			)}
 			<p className="tips__tip">
-				No results from the store you want? The server may have had some trouble
-				retrieving data from the store. Try searching the item again to see if you
-				can get some new results this time and make sure your internet service is
-				strong.
+				The server may have had some trouble retrieving data from the store. Try
+				searching the item again to see if you can get some new results this time
+				and make sure your internet service is strong.
 			</p>
 		</section>
 	);
