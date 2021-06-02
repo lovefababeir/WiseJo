56cfa10a672e7dbf8de21d3976f49366db467714
Love Fababeir <love.fababeir@hotmail.com>
2021-06-02 19:56:42 -0400

    Changed longos endpoint to added to the database instead of the js in the data folder. Created promise functions in the promiseFunction folders which are used to connect to the database.

--- server/routes/itemSearch.js
+++ server/routes/itemSearch.js
@@ -4,60 +4,124 @@ const longos = require("../webScrapingFunctions/gateway");
 const sobeys = require("../webScrapingFunctions/sobeys");
 const walmart = require("../webScrapingFunctions/walmart");
 const nofrills = require("../webScrapingFunctions/nofrills");
-const searchHistory = require("../data/searchResults");
+const ItemResults = require("../models/itemResults.js");
+const UserResults = require("../models/userResults.js");
+const mongoose = require("mongoose");
+const promiseFcn = require("../promiseFunctions/promiseFunctions");
+
+const searchDate = timestamp => {
+	let dateSubmitted = new Date(timestamp);
+	return {
+		year: dateSubmitted.getFullYear(),
+		month: dateSubmitted.getMonth() + 1,
+		day: dateSubmitted.getUTCDate(),
+	};
+};
 
 router.get("/longos/:item/:time", (req, res) => {
 	const item = req.params.item;
-	const time = parseInt(req.params.time) - 3600000 * 4;
-	const moreInfo = req.query;
+	const time = parseInt(req.params.time);
+	const userlocation = req.query.userlocation;
+	const userid = "lovefababeir";
+	const store = "Longo's";
+	console.log(userlocation);
 
-	const alreadySearched = searchHistory.filter(record => {
-		return (
-			record.search.toLowerCase() == item.toLowerCase() &&
-			record.searchResults[0].store === "Longo's"
-		);
-	});
-	const prevSearch = alreadySearched ? alreadySearched.pop() : "";
-
-	if (prevSearch) {
-		const newSearch = {
-			...prevSearch,
-			...moreInfo,
-			time: time,
-		};
-		searchHistory.push(newSearch);
-
-		res.status(200).json(newSearch);
-	} else {
-		longos
-			.store(item)
-			.then(result => {
-				if (!result.length) {
-					res
-						.status(400)
-						.send(
-							"No results found. Please check that the spelling of the item is correct or try again later.",
+	//Checks if there is already an up to date(same day) result.
+	ItemResults.find({
+		searchItem: item,
+		store: "Longo's",
+		date: searchDate(time),
+	})
+		.exec()
+		.then(result => {
+			//if there is already something on record, copy it onto the UserResults collection
+			if (result.length) {
+				const data = result[0];
+				promiseFcn
+					.createUserCopy(data, userid, userlocation, time)
+					.then(result => {
+						console.log("Recorded in userResults");
+						res
+							.status(201)
+							.json({ message: "Found data already on record", data: result });
+					})
+					.catch(error => {
+						res.status(500).json({
+							message: `Found something from a previous search today but could not log user data: ${error}`,
+							data: userData,
+						});
+						console.log(
+							`Found something from a previous search today but could not log user data: ${error}`,
 						);
-				} else {
-					const newData = {
-						...moreInfo,
-						time: time,
-						search: item,
-						searchResults: result,
-					};
-					searchHistory.push(newData);
-					res.status(200).json(newData);
-				}
-			})
-			.catch(err => {
-				console.log(`Could not complete Longos search for ${item}: ${err}`);
-				res
-					.status(500)
-					.json(
-						`Could not complete Longos search for ${item}. Internal issue with Longos function: ${err}`,
-					);
-			});
-	}
+					});
+				//if there is nothing on record, conduct a new search
+			} else {
+				console.log("No record of search yet.  Website search requested");
+				longos
+					.store(item)
+					.then(result => {
+						console.log("-----GOT RESULTS FROM WEBSITE------");
+						//If there were no results from the search then don't log the research and just return a message.
+						if (!result.length) {
+							res
+								.status(400)
+								.send(
+									"No results found. Please check that the spelling of the item is correct or try again later.",
+								);
+						} else {
+							//if search was succesful with at least one result,
+							//add it to the UserResults collection
+							promiseFcn
+								.recordNewSearchUserCopy(
+									userid,
+									userlocation,
+									item,
+									time,
+									searchDate(time),
+									store,
+									result,
+								)
+								.then(result => {
+									console.log("Recorded in userResults");
+								})
+								.catch(error => {
+									console.log(`Could not log user data: ${error}`);
+								});
+							//add it to the ItemsResults collection
+							promiseFcn
+								.recordNewSearch(item, time, searchDate(time), store, result)
+								.then(result => {
+									res.status(201).json({
+										message: "Successful search on the item.",
+										data: result,
+									});
+								})
+								.catch(error => {
+									res.status(500).json({
+										message: "Could not created a record of the search results",
+										error: error,
+									});
+								});
+						}
+					})
+					.catch(err => {
+						console.log(`Could not complete Longo's search for ${item}: ${err}`);
+						res
+							.status(500)
+							.json(
+								`Could not complete Longo's search for ${item}. Internal issue with Longo's function: ${err}`,
+							);
+					});
+			}
+		})
+		.catch(err => {
+			console.log(
+				`Could not conduct a search for the item in the database: ${err}`,
+			);
+			res
+				.status(500)
+				.json(`Had problems with checking history of searches for ${item}.`);
+		});
 });
 
 router.get("/sobeys/:item/:time", (req, res) => {
