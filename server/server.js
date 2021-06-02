9e009f49f3512ba547485b735c0e4c5a4e908268
Love Fababeir <love.fababeir@hotmail.com>
2021-06-02 05:36:42 -0400

    Connected to mongo database through mongoose.

--- server/server.js
+++ server/server.js
@@ -3,17 +3,34 @@ const app = express();
 const cors = require("cors");
 const receipts = require("./routes/receipts");
 const itemSearch = require("./routes/itemSearch");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT || 8080;
+
 //middleware
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
 
+//HTTP request logger
+app.use(morgan("tiny"));
+
+//Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+	console.log("-----Mongoose is connected!-----");
+});
+
+//Routes
 app.use("/itemSearch", itemSearch);
 app.use("/receipts", receipts);
 
 app.listen(PORT, () => {
-	console.log("Capstone");
+	console.log(`Capstone server is starting at ${PORT}`);
 });
