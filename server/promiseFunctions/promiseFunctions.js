75036f573577129f7720ea5d669aa458f70b4a7b
Love Fababeir <love.fababeir@hotmail.com>
2021-06-02 19:57:26 -0400

    Changed longos endpoint to added to the database instead of the js in the data folder. Created promise functions in the promiseFunction folders which are used to connect to the database.

---
+++ server/promiseFunctions/promiseFunctions.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const UserResults = require("../models/userResults.js");
+const ItemResults = require("../models/itemResults.js");
+
+const createUserCopy = (data, userid, userlocation, time) => {
+	const userResultsData = {
+		_id: mongoose.Types.ObjectId(),
+		userid: userid,
+		userlocation: userlocation,
+		time: time,
+		searchItem: data.searchItem,
+		searchTime: data.searchTime,
+		date: data.date,
+		store: data.store,
+		searchResults: data.searchResults,
+	};
+	console.log("found something");
+	const userData = new UserResults(userResultsData);
+	return userData.save();
+};
+
+const recordNewSearch = (item, time, searchDate, store, result) => {
+	const itemResultsData = {
+		_id: new mongoose.Types.ObjectId(),
+		searchItem: item,
+		searchTime: time,
+		date: searchDate,
+		store: store,
+		searchResults: result,
+	};
+
+	const itemData = new ItemResults(itemResultsData);
+	return itemData.save();
+};
+
+const recordNewSearchUserCopy = (
+	userid,
+	userlocation,
+	item,
+	time,
+	searchDate,
+	store,
+	result,
+) => {
+	const userResultsData = {
+		userid: userid,
+		userlocation: userlocation,
+		time: time,
+		_id: new mongoose.Types.ObjectId(),
+		searchItem: item,
+		searchTime: time,
+		date: searchDate,
+		store: store,
+		searchResults: result,
+	};
+	const userData = new UserResults(userResultsData);
+	return userData.save();
+};
+
+module.exports = { createUserCopy, recordNewSearch, recordNewSearchUserCopy };
