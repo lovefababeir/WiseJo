5dbf8f77ffc2af65f211dbf443260704914ef3f4
Love Fababeir <love.fababeir@hotmail.com>
2021-06-02 20:29:43 -0400

    Created general function to execute the search and brought it outside of the endpoint. Made endpoint function also general since each store has the same endpoint just different names of the stores and different store functions are called.

--- server/routes/itemSearch.js
+++ server/routes/itemSearch.js
@@ -9,65 +9,70 @@ const UserResults = require("../models/userResults.js");
 const mongoose = require("mongoose");
 const promiseFcn = require("../promiseFunctions/promiseFunctions");
 
-const searchDate = timestamp => {
-	let dateSubmitted = new Date(timestamp);
-	return {
+const conductSearch = async (
+	storeFunction,
+	item,
+	time,
+	userlocation,
+	userid,
+	store,
+) => {
+	//convert timestamp to date
+	const dateSubmitted = new Date(time);
+	const searchDate = {
 		year: dateSubmitted.getFullYear(),
 		month: dateSubmitted.getMonth() + 1,
 		day: dateSubmitted.getUTCDate(),
 	};
-};
-
-router.get("/longos/:item/:time", (req, res) => {
-	const item = req.params.item;
-	const time = parseInt(req.params.time);
-	const userlocation = req.query.userlocation;
-	const userid = "lovefababeir";
-	const store = "Longo's";
-	console.log(userlocation);
 
 	//Checks if there is already an up to date(same day) result.
-	ItemResults.find({
+	const responseData = await ItemResults.find({
 		searchItem: item,
-		store: "Longo's",
-		date: searchDate(time),
+		store: store,
+		date: searchDate,
 	})
 		.exec()
 		.then(result => {
 			//if there is already something on record, copy it onto the UserResults collection
 			if (result.length) {
 				const data = result[0];
-				promiseFcn
+				return promiseFcn
 					.createUserCopy(data, userid, userlocation, time)
 					.then(result => {
 						console.log("Recorded in userResults");
-						res
-							.status(201)
-							.json({ message: "Found data already on record", data: result });
+						return {
+							code: 201,
+							jsonData: { message: "Found data already on record", data: result },
+						};
 					})
 					.catch(error => {
-						res.status(500).json({
-							message: `Found something from a previous search today but could not log user data: ${error}`,
-							data: userData,
-						});
 						console.log(
 							`Found something from a previous search today but could not log user data: ${error}`,
 						);
+						return {
+							code: 500,
+							jsonData: {
+								message: `Found something from a previous search today but could not log user data: ${error}`,
+								data: userData,
+							},
+						};
 					});
 				//if there is nothing on record, conduct a new search
 			} else {
 				console.log("No record of search yet.  Website search requested");
-				longos
+				return storeFunction
 					.store(item)
 					.then(result => {
 						console.log("-----GOT RESULTS FROM WEBSITE------");
 						//If there were no results from the search then don't log the research and just return a message.
 						if (!result.length) {
-							res
-								.status(400)
-								.send(
-									"No results found. Please check that the spelling of the item is correct or try again later.",
-								);
+							return {
+								code: 400,
+								jsonData: {
+									message:
+										"No results found. Please check that the spelling of the item is correct or try again later.",
+								},
+							};
 						} else {
 							//if search was succesful with at least one result,
 							//add it to the UserResults collection
@@ -77,7 +82,7 @@ router.get("/longos/:item/:time", (req, res) => {
 									userlocation,
 									item,
 									time,
-									searchDate(time),
+									searchDate,
 									store,
 									result,
 								)
@@ -88,194 +93,96 @@ router.get("/longos/:item/:time", (req, res) => {
 									console.log(`Could not log user data: ${error}`);
 								});
 							//add it to the ItemsResults collection
-							promiseFcn
-								.recordNewSearch(item, time, searchDate(time), store, result)
+							return promiseFcn
+								.recordNewSearch(item, time, searchDate, store, result)
 								.then(result => {
-									res.status(201).json({
-										message: "Successful search on the item.",
-										data: result,
-									});
+									return {
+										code: 201,
+										jsonData: {
+											message: "Successful search on the item.",
+											data: result,
+										},
+									};
 								})
 								.catch(error => {
-									res.status(500).json({
-										message: "Could not created a record of the search results",
-										error: error,
-									});
+									return {
+										code: 500,
+										jsonData: {
+											message: "Could not created a record of the search results",
+											error: error,
+										},
+									};
 								});
 						}
 					})
-					.catch(err => {
-						console.log(`Could not complete Longo's search for ${item}: ${err}`);
-						res
-							.status(500)
-							.json(
-								`Could not complete Longo's search for ${item}. Internal issue with Longo's function: ${err}`,
-							);
+					.catch(error => {
+						console.log(`Could not complete ${store} search for ${item}: ${error}`);
+						return {
+							code: 500,
+							jsonData: {
+								message: `Could not complete ${store} search for ${item}. Internal issue with ${store} function`,
+								error: error,
+							},
+						};
 					});
 			}
+		})
+		.then(response => {
+			return response;
 		})
 		.catch(err => {
 			console.log(
 				`Could not conduct a search for the item in the database: ${err}`,
 			);
-			res
-				.status(500)
-				.json(`Had problems with checking history of searches for ${item}.`);
+			return {
+				code: 500,
+				jsonData: {
+					message: `Had problems with checking history of searches for ${item}.`,
+					error: error,
+				},
+			};
 		});
-});
+	return responseData;
+};
 
-router.get("/sobeys/:item/:time", (req, res) => {
-	const item = req.params.item;
-	const time = parseInt(req.params.time) - 3600000 * 4;
-	const moreInfo = req.query;
+router.get("/:store/:time", async (req, res) => {
+	const time = parseInt(req.params.time);
+	const userlocation = req.query.userlocation;
+	const item = req.query.item;
+	const userid = "lovefababeir";
+	console.log(req.params.store);
+	const store =
+		req.params.store === "longos"
+			? "Longo's"
+			: req.params.store === "sobeys"
+			? "Sobeys"
+			: req.params.store === "walmart"
+			? "Walmart"
+			: req.params.store === "nofrills"
+			? "No Frills"
+			: "n/a";
+	const storeFunction =
+		req.params.store === "longos"
+			? longos
+			: req.params.store === "sobeys"
+			? sobeys
+			: req.params.store === "walmart"
+			? walmart
+			: req.params.store === "nofrills"
+			? nofrills
+			: "n/a";
+	console.log(userlocation);
 
-	const alreadySearched = searchHistory.filter(record => {
-		return (
-			record.search.toLowerCase() == item.toLowerCase() &&
-			record.searchResults[0].store === "Sobeys"
-		);
-	});
-	const prevSearch = alreadySearched ? alreadySearched.pop() : "";
-
-	if (prevSearch) {
-		const newSearch = {
-			...prevSearch,
-			...moreInfo,
-			time: time,
-		};
-		searchHistory.push(newSearch);
-
-		res.status(200).json(newSearch);
-	} else {
-		sobeys
-			.store(item)
-			.then(result => {
-				if (!result.length) {
-					res
-						.status(400)
-						.send(
-							"No results found. Please check that the spelling of the item is correct or try again later.",
-						);
-				} else {
-					const newData = {
-						...moreInfo,
-						time: time,
-						search: item,
-						searchResults: result,
-					};
-					searchHistory.push(newData);
-					res.status(200).json(newData);
-				}
-			})
-			.catch(err => {
-				console.log(`Could not complete Sobeys search for ${item}: ${err}`);
-				res
-					.status(500)
-					.json(
-						`Could not complete Sobeys search for ${item}. Internal issue with Sobeys function: ${err}`,
-					);
-			});
-	}
-});
-
-router.get("/walmart/:item/:time", (req, res) => {
-	const item = req.params.item;
-	const time = parseInt(req.params.time) - 3600000 * 4;
-	const moreInfo = req.query;
-
-	const alreadySearched = searchHistory.filter(record => {
-		return (
-			record.search.toLowerCase() == item.toLowerCase() &&
-			record.searchResults[0].store === "Walmart"
-		);
-	});
-	const prevSearch = alreadySearched ? alreadySearched.pop() : "";
-
-	if (prevSearch) {
-		const newSearch = { ...prevSearch, ...moreInfo, time: time };
-		searchHistory.push(newSearch);
-		res.status(200).json(newSearch);
-	} else {
-		walmart
-			.store(item)
-			.then(result => {
-				if (!result.length) {
-					res
-						.status(400)
-						.send(
-							"No results found. Please check that the spelling of the item is correct or try again later.",
-						);
-				} else {
-					const newData = {
-						...moreInfo,
-						time: time,
-						search: item,
-						searchResults: result,
-					};
-					searchHistory.push(newData);
-					res.status(200).json(newData);
-				}
-			})
-			.catch(err => {
-				console.log(`Could not complete Walmart search for ${item}: ${err}`);
-				res
-					.status(500)
-					.json(
-						`Could not complete Walmart search for ${item}. Internal issue with Walmart function: ${err}`,
-					);
-			});
-		// res.status(200).send("Not available at the moment. Please try again later");
-	}
-});
-
-router.get("/nofrills/:item/:time", (req, res) => {
-	const item = req.params.item;
-	const time = parseInt(req.params.time) - 3600000 * 4;
-	const moreInfo = req.query;
-
-	const alreadySearched = searchHistory.filter(record => {
-		return (
-			record.search.toLowerCase() == item.toLowerCase() &&
-			record.searchResults[0].store === "No Frills"
-		);
-	});
-	const prevSearch = alreadySearched ? alreadySearched.pop() : "";
-
-	if (prevSearch) {
-		const newSearch = { ...prevSearch, ...moreInfo, time: time };
-
-		searchHistory.push(newSearch);
-		res.status(200).json(newSearch);
-	} else {
-		nofrills
-			.store(item)
-			.then(result => {
-				if (!result.length) {
-					res
-						.status(400)
-						.send(
-							"No results found. Please check that the spelling of the item is correct or try again later.",
-						);
-				} else {
-					const newData = {
-						...moreInfo,
-						time: time,
-						search: item,
-						searchResults: result,
-					};
-					searchHistory.push(newData);
-					res.status(200).json(newData);
-				}
-			})
-			.catch(err => {
-				console.log(`Could not complete No Frills search for ${item}: ${err}`);
-				res
-					.status(500)
-					.json(
-						`Could not complete No Frills search for ${item}. Internal issue with No Frills function: ${err}`,
-					);
-			});
-	}
+	const responseData = await conductSearch(
+		storeFunction,
+		item,
+		time,
+		userlocation,
+		userid,
+		store,
+	);
+	console.log("inside endpoint", responseData.code, responseData.jsonData);
+	res.status(responseData.code).json(responseData.jsonData);
 });
 
 router.get("/history", (req, res) => {
