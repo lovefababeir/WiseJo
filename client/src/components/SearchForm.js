b802727311664cb90974992dcb3b5d1e1e6fe107
Love Fababeir <love.fababeir@hotmail.com>
2021-06-03 04:02:52 -0400

    Disabled form when there is a submission in progress.

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -10,15 +10,21 @@ const SearchForm = ({ submitHandler, loading }) => {
 			<form className="form" action="submit" onSubmit={submitHandler}>
 				<div className="form__box">
 					<label className="form__label">Select a grocery store</label>
-					<select name="groceryStore" id="storeList">
+					<select name="groceryStore" id="storeList" disabled={loading}>
 						<option value="Longo's">Longo's</option>
 						<option value="No Frills">No Frills</option>
 						<option value="Sobeys">Sobeys</option>
 						<option value="Walmart">Walmart</option>
 					</select>
 					<label className="form__label">Search an item</label>
-					<input type="text" id="search" ref={inputRef} autoComplete="off"></input>
-					<button type="submit" id="submitBtn">
+					<input
+						type="text"
+						id="search"
+						ref={inputRef}
+						autoComplete="off"
+						disabled={loading}
+					></input>
+					<button type="submit" id="submitBtn" disabled={loading}>
 						search
 					</button>
 				</div>
