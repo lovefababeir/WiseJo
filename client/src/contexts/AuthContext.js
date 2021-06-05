87a58124202a9defee168a8bf64ba0acc1360f6b
Love Fababeir <love.fababeir@hotmail.com>
2021-06-05 14:49:12 -0400

    Added function to create token for user to access the backend.

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import firebase from "../firebase";
-
 const AuthContext = React.createContext();
-
 export const useAuth = () => {
 	return useContext(AuthContext);
 };
@@ -10,20 +8,30 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState("");
 	const [loading, setLoading] = useState(true);
-
 	const logout = () => {
 		return firebase.auth().signOut();
 	};
-
 	useEffect(() => {
 		const unsubscribe = firebase.auth().onAuthStateChanged(user => {
 			setCurrentUser(user);
 			setLoading(false);
 		});
+
 		return unsubscribe;
 	}, []);
+	const createToken = async () => {
+		const token = await currentUser.getIdToken();
+		const payloadHeader = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		};
+		return payloadHeader;
+	};
 
-	const value = { currentUser, logout };
+	console.log("current user", currentUser);
+	const value = { currentUser, logout, createToken };
 	return (
 		<AuthContext.Provider value={value}>
 			{!loading && children}
