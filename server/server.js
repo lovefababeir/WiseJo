1ee8cded6cdc5f546c8e516f0bf717bc9e1db46b
Love Fababeir <love.fababeir@hotmail.com>
2021-06-05 14:46:42 -0400

    Added authenticator for the firebase token to allow access to wisejo users.

--- server/server.js
+++ server/server.js
@@ -5,9 +5,19 @@ const receipts = require("./routes/receipts");
 const itemSearch = require("./routes/itemSearch");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const decodeIDToken = require("./authenticateToken.js");
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 8080;
+
+//Mongo DB
+mongoose.connect(process.env.MONGODB_URI_ITEMSEARCH, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+	console.log("-----Mongoose is connected to ITEMSEARCH!-----");
+});
 
 //middleware
 app.use(express.json());
@@ -18,14 +28,8 @@ app.use(cors());
 //HTTP request logger
 app.use(morgan("tiny"));
 
-//Mongo DB
-mongoose.connect(process.env.MONGODB_URI_ITEMSEARCH, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-mongoose.connection.on("connected", () => {
-	console.log("-----Mongoose is connected to ITEMSEARCH!-----");
-});
+//Athenticate token with firebase
+app.use(decodeIDToken);
 
 app.use("/itemSearch", itemSearch);
 app.use("/receipts", receipts);
