35714afd8ca2165cda0147066906626a2c45dfbd
Love Fababeir <love.fababeir@hotmail.com>
2021-06-06 23:08:20 -0400

    Removed code from Receipt Edit and placed it into its own component.

---
+++ client/src/components/ReceiptEditStoreDetails.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+const ReceiptEditStoreDetails = ({
+	purchaseData,
+	changePurchaseData,
+	dateToString,
+	changeDate,
+	store,
+	date,
+}) => {
+	const { storeID, contact, address, manager, cashier } = purchaseData;
+	const today = new Date();
+	const todaysDate = `${today.getFullYear()}-${dateToString(
+		today.getMonth() + 1,
+	)}-${dateToString(today.getDay())}`;
+
+	return (
+		<div className="storeDetails">
+			<div className="receipt-header">
+				<p className="storeDetails__store">{store}</p>
+			</div>
+			<span>
+				<label htmlFor="storeID">Store Location: </label>
+				<input
+					id="storeID"
+					name="storeID"
+					type="text"
+					placeholder={storeID}
+					defaultValue={storeID}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+			<span>
+				<label htmlFor="purchaseDate">Purchase Date:</label>
+				<input
+					type="date"
+					id="purchaseDate"
+					name="purchaeDate"
+					max={todaysDate}
+					required
+					defaultValue={`${date.year}-${dateToString(date.month)}-${dateToString(
+						date.day,
+					)}`}
+					onChange={e => {
+						changeDate(e);
+					}}
+				/>
+			</span>
+
+			<span>
+				<label htmlFor="contact">Contact: </label>
+				<input
+					id="contact"
+					name="contact"
+					type="text"
+					placeholder={contact}
+					defaultValue={contact}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+
+			<span>
+				<label htmlFor="address">Address: </label>
+				<input
+					id="address"
+					name="address"
+					type="text"
+					placeholder={address}
+					defaultValue={address}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+
+			{manager && (
+				<span>
+					<label htmlFor="manager">Manager: </label>
+					<input
+						id="manager"
+						name="manager"
+						type="text"
+						placeholder={manager}
+						defaultValue={manager}
+						onChange={e => changePurchaseData(e)}
+					></input>
+				</span>
+			)}
+
+			<span>
+				<label htmlFor="cashier">Cashier: </label>
+				<input
+					id="cashier"
+					name="cashier"
+					type="text"
+					placeholder={cashier}
+					defaultValue={cashier}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+		</div>
+	);
+};
+
+export default ReceiptEditStoreDetails;
