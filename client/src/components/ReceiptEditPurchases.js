890b54cd20306ee2242b391848630ba7aa5f88d2
Love Fababeir <love.fababeir@hotmail.com>
2021-06-06 23:08:00 -0400

    Removed code from Receipt Edit and placed it into its own component.

---
+++ client/src/components/ReceiptEditPurchases.js
@@ -0,0 +1,81 @@
+import React from "react";
+import save from "../assets/images/save.svg";
+import deleteIcon from "../assets/images/delete.svg";
+import close from "../assets/images/close.svg";
+
+const ReceiptEditPurchases = ({
+	purchaseData,
+	changePurchase,
+	deleteHandler,
+	cancelChangesHandler,
+	changePurchaseData,
+	receiptID,
+}) => {
+	const { purchases, total, subtotal } = purchaseData;
+	return (
+		<div className="purchaseDetails">
+			<p className="purchaseDetails__item-title">Items Purchased:</p>
+
+			{purchases.map((item, i) => {
+				return (
+					<input
+						name={i}
+						type="text"
+						placeholder={item}
+						defaultValue={item}
+						onChange={e => changePurchase(e)}
+						key={[`purchases`, receiptID, i].join("_")}
+					></input>
+				);
+			})}
+			<span>
+				<label>Subtotal: $</label>
+				<input
+					id="subtotal"
+					name="subtotal"
+					type="number"
+					step="0.01"
+					placeholder={subtotal}
+					defaultValue={subtotal}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+			<span>
+				<label>Total Spent: $</label>
+				<input
+					id="total"
+					name="total"
+					type="number"
+					step="0.01"
+					placeholder={total}
+					defaultValue={total}
+					onChange={e => changePurchaseData(e)}
+				></input>
+			</span>
+			<span className="button-container">
+				<button
+					className="edit"
+					type="button"
+					onClick={() => deleteHandler(receiptID)}
+				>
+					<img src={deleteIcon} className="edit-icon" />
+					Delete
+				</button>
+				<button type="submit" className="edit edit--purchaseDetails">
+					<img src={save} className="save-icon" />
+					Save
+				</button>
+				<button
+					type="button"
+					className="edit"
+					onClick={() => cancelChangesHandler(receiptID)}
+				>
+					<img src={close} className="edit-icon" />
+					Cancel
+				</button>
+			</span>
+		</div>
+	);
+};
+
+export default ReceiptEditPurchases;
