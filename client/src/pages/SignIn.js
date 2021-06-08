7a3b606c98ea1219984451d201c4b04f34963eda
Love Fababeir <love.fababeir@hotmail.com>
2021-06-08 15:17:22 -0400

    Added more styles and edited sign in options. Added redirects file for netlify to know where to direct the internal pages of the sight.

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -18,10 +18,12 @@ const uiConfig = {
 	signInOptions: [
 		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-		firebase.auth.TwitterAuthProvider.PROVIDER_ID,
 		firebase.auth.GithubAuthProvider.PROVIDER_ID,
-		firebase.auth.EmailAuthProvider.PROVIDER_ID,
 		firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+		{
+			provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+		},
 	],
 };
 
