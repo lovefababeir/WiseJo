4703bb9edb55354602e2aa95d9bf1bf4945a0f81
Love Fababeir <love.fababeir@hotmail.com>
2021-06-09 23:55:11 -0400

    Added tips bar at the bottom of the selected receipt. Will only show when a receipt is selected.

--- client/src/components/ReceiptSelected.js
+++ client/src/components/ReceiptSelected.js
@@ -2,6 +2,7 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./ReceiptSelected.scss";
 import edit from "../assets/images/edit.svg";
+import TipsSection from "./TipsSection.js";
 
 const ReceiptSelected = ({
 	purchaseData,
@@ -22,46 +23,49 @@ const ReceiptSelected = ({
 	} = purchaseData;
 
 	return (
-		<div className="receipts-selected">
-			<div className="storeDetails">
-				<div className="receipt-header">
-					<p className="storeDetails__store">{store}</p>
-					<button
-						className="edit edit--purchaseDetails"
-						onClick={() => editModeHandler()}
-					>
-						<img
-							src={edit}
-							className="edit-icon"
-							alt="Click here to edit the contents of the receipt"
-						/>
-					</button>
-				</div>
-				<p className="storeDetails__detail">Store Location: {storeID}</p>
-				<p className="storeDetails__detail">
-					Purchase Date: {monthNames[date.month - 1]} {date.day} {date.year}
-				</p>
-				{contact && <p className="storeDetails__detail">Contact: {contact}</p>}
-				<p className="storeDetails__detail">{address}</p>
+		<>
+			<div className="receipts-selected">
+				<div className="storeDetails">
+					<div className="receipt-header">
+						<p className="storeDetails__store">{store}</p>
+						<button
+							className="edit edit--purchaseDetails"
+							onClick={() => editModeHandler()}
+						>
+							<img
+								src={edit}
+								className="edit-icon"
+								alt="Click here to edit the contents of the receipt"
+							/>
+						</button>
+					</div>
+					<p className="storeDetails__detail">Store Location: {storeID}</p>
+					<p className="storeDetails__detail">
+						Purchase Date: {monthNames[date.month - 1]} {date.day} {date.year}
+					</p>
+					{contact && <p className="storeDetails__detail">Contact: {contact}</p>}
+					<p className="storeDetails__detail">{address}</p>
 
-				{manager && <p className="storeDetails__detail">Manager: {manager}</p>}
-				{cashier && <p className="storeDetails__detail">Cashier: {cashier}</p>}
-			</div>
-			<div className="purchaseDetails">
-				<div className="receipt-header">
-					<p className="purchaseDetails__item-title">Items Purchased:</p>
+					{manager && <p className="storeDetails__detail">Manager: {manager}</p>}
+					{cashier && <p className="storeDetails__detail">Cashier: {cashier}</p>}
 				</div>
-				{purchases.map(item => {
-					return (
-						<p className="purchaseDetails__item" key={uuidv4()}>
-							{item}
-						</p>
-					);
-				})}
-				<p className="purchaseDetails__subtotal">Subtotal: ${subtotal}</p>
-				<p className="purchaseDetails__total">Total Spent: ${total}</p>
+				<div className="purchaseDetails">
+					<div className="receipt-header">
+						<p className="purchaseDetails__item-title">Items Purchased:</p>
+					</div>
+					{purchases.map(item => {
+						return (
+							<p className="purchaseDetails__item" key={uuidv4()}>
+								{item}
+							</p>
+						);
+					})}
+					<p className="purchaseDetails__subtotal">Subtotal: ${subtotal}</p>
+					<p className="purchaseDetails__total">Total Spent: ${total}</p>
+				</div>
 			</div>
-		</div>
+			<TipsSection page="receiptList" />
+		</>
 	);
 };
 
