cf16602556bfb5cf29e28a37ed15a4865fdaded9
Love Fababeir <love.fababeir@hotmail.com>
2021-06-11 01:36:12 -0400

    Added the form to change the user's profile name and used bootstrap to make it collapsable.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,23 +1,19 @@
 import React, { useRef, useState } from "react";
-import { Card, Form, Button, Alert } from "react-bootstrap";
+import { Card, Form, Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext.js";
 import ContactForm from "../components/ProfileContactForm";
 import "./Profile.scss";
+import Collapse from "react-bootstrap/Collapse";
 
 const EditProfile = () => {
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const passwordConfirmRef = useRef();
+	const usernameRef = useRef();
 	const { currentUser } = useAuth();
-	const [error, setError] = useState("");
-	const [message, setMessage] = useState("");
-	const [loading, setLoading] = useState(false);
 	const [edit, setEdit] = useState(false);
 
 	async function handleSubmit(e) {
 		e.preventDefault();
+		setEdit(!edit);
 	}
-	console.log(currentUser);
 	return (
 		<section className="profile">
 			{" "}
@@ -25,36 +21,60 @@ const EditProfile = () => {
 				<Card.Body>
 					<h1 className="signin__appName">WiseJo</h1>
 					<h2 className="accountForms__title">Profile</h2>
-					{error && <Alert variant="danger">{error}</Alert>}
-					{message && <Alert variant="warning">{message}</Alert>}
-					<div class="profile__details">
-						<p className="profile__user">
-							Username: <span>{currentUser.displayName}</span>
+
+					<Collapse in={!edit}>
+						<div class="profile__details" id="profile__details">
+							<p className="profile__user">
+								Username: <span>{currentUser.displayName}</span>
+							</p>
+							<p className="profile__user">
+								Email: <span>{currentUser.email}</span>
+							</p>
+						</div>
+					</Collapse>
+					{!edit && (
+						<p
+							onClick={() => {
+								setEdit(!edit);
+							}}
+							aria-controls="edit-users-profile profile__details connect-form"
+							aria-expanded={edit}
+							className="editProfile__button"
+						>
+							Edit Profile
 						</p>
-						<p className="profile__user">
-							Email: <span>{currentUser.email}</span>
-						</p>
-					</div>
-					{edit && (
-						<Form onSubmit={handleSubmit} className="accountForms__form">
-							<Form.Group id="email">
-								<Form.Label>Email</Form.Label>
-								<Form.Control type="email" ref={emailRef} required />
+					)}
+					<Collapse in={edit}>
+						<Form
+							onSubmit={handleSubmit}
+							className="accountForms__form"
+							id="edit-users-profile"
+						>
+							<Form.Group id="edit__username">
+								<Form.Label>Username</Form.Label>
+								<Form.Control
+									type="text"
+									ref={usernameRef}
+									required
+									defaultValue={currentUser.displayName}
+								/>
 							</Form.Group>
-							<Form.Group id="password">
-								<Form.Label>Password</Form.Label>
-								<Form.Control type="password" ref={passwordRef} required />
-							</Form.Group>
-							<Form.Group id="password-confirm">
-								<Form.Label>Password Confirmation</Form.Label>
-								<Form.Control type="password" ref={passwordConfirmRef} required />
-							</Form.Group>
-							<Button disabled={loading} className="w-100" type="submit">
-								Sign Up
+
+							<Button className="w-100" type="submit">
+								Save
 							</Button>
 						</Form>
-					)}
-					<ContactForm currentUser={currentUser} />
+					</Collapse>
+					<Collapse in={!edit}>
+						<div
+							id="connect-form"
+							style={{
+								width: "100%",
+							}}
+						>
+							<ContactForm currentUser={currentUser} />
+						</div>
+					</Collapse>
 				</Card.Body>
 			</Card>
 		</section>
