b1ea15265083414aef5204b69129e362915e1565
Love Fababeir <love.fababeir@hotmail.com>
2021-06-14 05:38:22 -0400

    Removed Collapse which was used only because the form was a part of the profile page. It is no longer needed.

--- client/src/components/ProfileContactForm.js
+++ client/src/components/ProfileContactForm.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import "./ProfileContactForm.scss";
-import Collapse from "react-bootstrap/Collapse";
 import ButtonDashboard from "./ButtonDashboard";
 
 const ContactForm = ({ currentUser, edit }) => {
@@ -10,48 +9,46 @@ const ContactForm = ({ currentUser, edit }) => {
 		return <p>Message sent! We'll get back to you as soon as possible.</p>;
 	}
 	return (
-		<Collapse in={!edit}>
-			<div
-				id="connect-form"
-				style={{
-					width: "100%",
-				}}
-			>
-				<form onSubmit={handleSubmit} className="connect-form">
-					<h2 className="connect-form__title">Need help? Contact Us</h2>
-					<label htmlFor="email" className="connect-form__label">
-						Email Address:
-					</label>
-					<input
-						id="email"
-						type="email"
-						name="email"
-						defaultValue={currentUser.email}
-						className="connect-form__textbox"
-						disabled={currentUser.email}
-					/>
-					<ValidationError prefix="Email" field="email" errors={state.errors} />
-					<label htmlFor="email" className="connect-form__label">
-						Message:
-					</label>
-					<textarea
-						id="message"
-						name="message"
-						className="connect-form__textbox connect-form__textbox--2"
-					/>
+		<div
+			id="connect-form"
+			style={{
+				width: "100%",
+			}}
+		>
+			<form onSubmit={handleSubmit} className="connect-form">
+				<h2 className="connect-form__title">Need help? Contact Us</h2>
+				<label htmlFor="email" className="connect-form__label">
+					Email Address:
+				</label>
+				<input
+					id="email"
+					type="email"
+					name="email"
+					defaultValue={currentUser.email}
+					className="connect-form__textbox"
+					disabled={currentUser.email}
+				/>
+				<ValidationError prefix="Email" field="email" errors={state.errors} />
+				<label htmlFor="email" className="connect-form__label">
+					Message:
+				</label>
+				<textarea
+					id="message"
+					name="message"
+					className="connect-form__textbox connect-form__textbox--2"
+				/>
 
-					<ValidationError prefix="Message" field="message" errors={state.errors} />
-					<button
-						type="submit"
-						disabled={state.submitting}
-						className="connect-form__button"
-					>
-						Submit
-					</button>
-				</form>
-				<ButtonDashboard />
-			</div>
-		</Collapse>
+				<ValidationError prefix="Message" field="message" errors={state.errors} />
+				<button
+					type="submit"
+					disabled={state.submitting}
+					className="connect-form__button"
+				>
+					Submit
+				</button>
+			</form>
+			<ButtonDashboard />
+		</div>
 	);
 };
 export default ContactForm;
