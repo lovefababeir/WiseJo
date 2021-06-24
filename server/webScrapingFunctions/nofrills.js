82660d1828fafd52d2d29821e48dc5d85652e369
Love Fababeir <love.fababeir@hotmail.com>
2021-06-24 02:37:03 -0400

    reduced timeout.

--- server/webScrapingFunctions/nofrills.js
+++ server/webScrapingFunctions/nofrills.js
@@ -6,7 +6,7 @@ const store = async function (searchWords) {
 	const options = { args, headless: true };
 	var browser = await puppeteer.launch(options);
 	var page = await browser.newPage();
-	await page.setDefaultTimeout(120000);
+	await page.setDefaultTimeout(30000);
 	await page.setViewport({ height: 1200, width: 960 });
 
 	await page.goto(`https://www.nofrills.ca/search?search-bar=${searchText}`);
