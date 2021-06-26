bef2b21c59b561cc2fb8a1f046147da1d7088a5b
Love Fababeir <love.fababeir@hotmail.com>
2021-06-25 23:56:22 -0400

    Created component that generates menu items and used it for the menu.

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,85 +1,41 @@
 import React from "react";
 import "./MenuBar.scss";
-import { NavLink } from "react-router-dom";
 import receipt from "../assets/images/receipt.svg";
 import camera from "../assets/images/camera.svg";
 import search from "../assets/images/shoppingCart.svg";
 import compare from "../assets/images/tags.svg";
 import home from "../assets/images/home.svg";
+import MenuBarItem from "./MenuBarItem";
 
 const MenuBar = () => {
 	return (
 		<nav className="menu-bar">
 			<ul className="menu">
-				<li>
-					<NavLink
-						to="/shop"
-						className="menu__item"
-						activeClassName="menu-item--selected"
-					>
-						<img
-							src={search}
-							className="menu__icon"
-							alt="Click here to search the prices of a specific item"
-						/>
-						<h4 className="menu__label">shop</h4>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						to="/compare"
-						className="menu__item"
-						activeClassName="menu-item--selected"
-					>
-						<img
-							src={compare}
-							className="menu__icon"
-							alt="Click here to show the results of your most recent search and compare the prices from different locations and sizes"
-						/>
-					</NavLink>
-					<h4 className="menu__label">compare</h4>
-				</li>
-				<li>
-					<NavLink
-						to="/"
-						exact
-						className="menu__item"
-						activeClassName="menu-item--selected"
-					>
-						<img
-							src={home}
-							className="menu__icon"
-							alt="Click here to go back to the home page."
-						/>
-					</NavLink>
-					<h4 className="menu__label">home</h4>
-				</li>
-				<li>
-					<NavLink
-						exact
-						to="/snap"
-						className="menu__item"
-						activeClassName="menu-item--selected"
-					>
-						<img
-							src={camera}
-							className="menu__icon menu__icon--3"
-							alt="Click here to add a new receipt"
-						/>
-					</NavLink>
-					<h4 className="menu__label">snap</h4>
-				</li>
-				<li>
-					<NavLink to="/track" activeClassName="menu-item--selected">
-						<img
-							src={receipt}
-							className="menu__icon menu__icon--4"
-							alt="Click here to check the history of your previous purchases"
-						/>
-					</NavLink>
-
-					<h4 className="menu__label">track</h4>
-				</li>
+				<MenuBarItem
+					img={search}
+					description="Click here to search the prices of a specific item"
+					page={"shop"}
+				/>
+				<MenuBarItem
+					img={compare}
+					description="Click here to show the results of your most recent search and compare the prices from different locations and sizes"
+					page={"compare"}
+				/>
+				<MenuBarItem
+					img={home}
+					description="Click here to go back to the home page."
+					page={"home"}
+				/>
+				<MenuBarItem
+					img={camera}
+					description="Click here to add a new receipt"
+					page={"snap"}
+				/>
+				<MenuBarItem
+					img={receipt}
+					description="Click here to add a new receipt"
+					page={"track"}
+				/>
 			</ul>
 		</nav>
 	);
