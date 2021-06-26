336758a4c4df23f158fe2a36b3d090473250c8d8
Love Fababeir <love.fababeir@hotmail.com>
2021-06-25 23:32:02 -0400

    Added route for endpoints used for that app stats.

---
+++ server/routes/admin.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const ItemResults = require("../models/itemRecord");
+const UserResults = require("../models/userRecord.js");
+const ReceiptCollection = require("../models/receipt");
+
+module.exports = router;
