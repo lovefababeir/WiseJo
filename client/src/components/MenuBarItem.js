f0ac0879b28ce341f1de0b9327e416587542ef21
Love Fababeir <love.fababeir@hotmail.com>
2021-06-25 23:49:15 -0400

    Created component for the items on the menu bar

---
+++ client/src/components/MenuBarItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const MenuBarItem = ({ img, description, page }) => {
+	return (
+		<li>
+			<NavLink
+				to={`/${page === "home" ? "" : page}`}
+				exact
+				className="menu__item"
+				activeClassName="menu-item--selected"
+			>
+				<img src={img} className="menu__icon" alt={description} />
+				<h4 className="menu__label">{page}</h4>
+			</NavLink>
+		</li>
+	);
+};
+
+export default MenuBarItem;
