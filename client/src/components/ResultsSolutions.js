a8505fbc85031fd8bd977eb19fbefdfeed923813
Love Fababeir <love.fababeir@hotmail.com>
2021-06-26 23:48:53 -0400

    Moved solutions to their own components.

--- client/src/components/ResultsSolutions.js
+++ client/src/components/ResultsSolutions.js
@@ -3,6 +3,10 @@ import "./ResultsSolutions.scss";
 import { useSpring, animated } from "react-spring";
 import TipsSection from "./TipsSection";
 import notavailable from "../assets/images/notavailable.png";
+import SolutionBest from "./SolutionBest";
+import SolutionStoreBest from "./SolutionStoreBest";
+import SolutionStoreSecond from "./SolutionStoreSecond";
+import SolutionSecondBest from "./SolutionSecondBest";
 
 const ResultsSolutions = props => {
 	const { list, currentLocation } = props;
@@ -97,138 +101,43 @@ const ResultsSolutions = props => {
 			{optionsInStore(currentLocation) && (
 				<>
 					<animated.div style={details}>
-						<div className="solution">
-							<img
-								className="solution__picture"
-								src={BEST.image || notavailable}
-								alt="Item of overall best choice"
-								onError={replaceImage}
-							/>
-							<div className="solution__details">
-								<p className="solution__advice">
-									Great news! We found the option that will get you more for your money
-									{optionsInStore(currentLocation) &&
-										bestinStore &&
-										" and you don't have to go to another store to get it!"}
-									.
-									{!bestinStore &&
-										savings > 0 &&
-										` You would save $${savings} if you buy this item from ${BEST.store} instead of ${BESTinLocation.store}. However it may take you another 15mins to drive there and make the purchase`}
-									{!optionsInStore(currentLocation) &&
-										!bestinStore &&
-										` Unfortunatey its not there at ${currentLocation}.`}
-								</p>
-								<h2 className="solution__detail solution__detail--store">
-									{BEST.store}
-								</h2>
-								<h2 className="solution__detail solution__detail--price">
-									{BEST.price}
-								</h2>
-								<h2 className="solution__detail solution__detail--capacity">
-									for {BEST.capacity}
-								</h2>
-								<h2 className="solution__detail solution__detail--solution">
-									Unit Rate: ${BEST.unitPrice.cost.toFixed(2)}/{BEST.unitPrice.mass}
-									{BEST.unitPrice.units}
-								</h2>
-							</div>
-						</div>
+						<SolutionBest
+							BEST={BEST}
+							notavailable={notavailable}
+							replaceImage={replaceImage}
+							bestinStore={bestinStore}
+							savings={savings}
+							BESTinLocation={BESTinLocation}
+							optionsInStore={optionsInStore}
+							currentLocation={currentLocation}
+						/>
 					</animated.div>
-
 					{currentLocation && inStoreResults && !bestinStore && (
 						<animated.div style={details2}>
-							<div className="solution">
-								<img
-									className="solution__picture"
-									src={BESTinLocation.image || notavailable}
-									alt="Best option in current location"
-									onError={replaceImage}
-								/>
-								<div className="solution__details">
-									<p className="solution__advice">
-										This is the item with the best unit price from where you are shopping
-										now.{" "}
-										{capacityDiff &&
-											`It also contains ${capacityDiff}g more than the next option.`}
-									</p>
-									<h2 className="solution__detail solution__detail--store">
-										{BESTinLocation.store}
-									</h2>
-									<h2 className="solution__detail solution__detail--price">
-										{BESTinLocation.price}
-									</h2>
-									<h2 className="solution__detail solution__detail--capacity">
-										{BESTinLocation.capacity}
-									</h2>
-									<h2 className="solution__detail solution__detail--unitRate">
-										Unit Rate: ${BESTinLocation.unitPrice.cost.toFixed(2)}/
-										{BESTinLocation.unitPrice.mass}
-										{BESTinLocation.unitPrice.units}
-									</h2>
-								</div>
-							</div>
+							<SolutionStoreBest
+								BESTinLocation={BESTinLocation}
+								notavailable={notavailable}
+								replaceImage={replaceImage}
+								capacityDiff={capacityDiff}
+							/>
 						</animated.div>
 					)}
 
 					{inStoreResults && curLocationOptions[1] && (
 						<animated.div style={details3} className="last-solution">
-							<div className="solution">
-								<img
-									className="solution__picture"
-									src={curLocationOptions[1].image || notavailable}
-									alt="Second best option in current lcoation"
-									onError={replaceImage}
-								/>
-								<div className="solution__details">
-									<p className="solution__advice">
-										This is the second best option in the store you are currently in. Not
-										bad for a back up plan!{" "}
-									</p>
-									<h2 className="solution__detail solution__detail--store">
-										{curLocationOptions[1].store}
-									</h2>
-									<h2 className="solution__detail solution__detail--price">
-										{curLocationOptions[1].price}
-									</h2>
-									<h2 className="solution__detail solution__detail--capacity">
-										{curLocationOptions[1].capacity}
-									</h2>
-									<h2 className="solution__detail solution__detail--unitRate">
-										Unit Rate: ${curLocationOptions[1].unitPrice.cost.toFixed(2)}/
-										{curLocationOptions[1].unitPrice.mass}
-										{curLocationOptions[1].unitPrice.units}
-									</h2>
-								</div>
-							</div>
+							<SolutionStoreSecond
+								curLocationOptions={curLocationOptions[1]}
+								replaceImage={replaceImage}
+								notavailable={notavailable}
+							/>
 						</animated.div>
 					)}
 					{!inStoreResults && (
 						<animated.div style={details3} className="last-solution">
-							<div className="solution">
-								<img
-									className="solution__picture"
-									src={unitRates[1].image}
-									alt="Second best option in current lcoation"
-									onError={replaceImage}
-								/>
-								<div className="solution__details">
-									<p className="solution__advice">This is the second best option.</p>
-									<h2 className="solution__detail solution__detail--store">
-										{unitRates[1].store}
-									</h2>
-									<h2 className="solution__detail solution__detail--price">
-										{unitRates[1].price}
-									</h2>
-									<h2 className="solution__detail solution__detail--capacity">
-										{unitRates[1].capacity}
-									</h2>
-									<h2 className="solution__detail solution__detail--unitRate">
-										Unit Rate: ${unitRates[1].unitPrice.cost.toFixed(2)}/
-										{unitRates[1].unitPrice.mass}
-										{unitRates[1].unitPrice.units}
-									</h2>
-								</div>
-							</div>
+							<SolutionSecondBest
+								unitRates={unitRates[1]}
+								replaceImage={replaceImage}
+							/>
 						</animated.div>
 					)}
 				</>
