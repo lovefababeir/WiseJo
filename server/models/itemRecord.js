63086e76c90342f878db9b4a812ed8689ae614f1
Love Fababeir <love.fababeir@hotmail.com>
2021-06-30 21:20:34 -0400

    Removed the object of every store and left the array of results.

--- server/models/itemRecord.js
+++ server/models/itemRecord.js
@@ -6,30 +6,12 @@ const itemRecord = mongoose.Schema({
 	searchTime: Number,
 	date: { year: Number, month: Number, day: Number },
 	store: String,
-	loblaws: {
-		searchResults: Array,
-		time: Number,
-	},
-	longos: {
-		searchResults: Array,
-		time: Number,
-	},
-	nofrills: {
-		searchResults: Array,
-		time: Number,
-	},
-	sobeys: {
-		searchResults: Array,
-		time: Number,
-	},
-	superstore: {
-		searchResults: Array,
-		time: Number,
-	},
-	walmart: {
-		searchResults: Array,
-		time: Number,
-	},
+	loblaws: Array,
+	longos: Array,
+	nofrills: Array,
+	sobeys: Array,
+	superstore: Array,
+	walmart: Array,
 });
 
 module.exports = mongoose.model("ItemRecord", itemRecord);
