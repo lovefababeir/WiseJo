6bfb81df69b95c41eeefd5a9e525914625276015
Love Fababeir <love.fababeir@hotmail.com>
2021-07-05 23:40:19 -0400

    Added section that displays numbers of users, members and guests.g

--- client/src/pages/AdminAccess.js
+++ client/src/pages/AdminAccess.js
@@ -19,7 +19,7 @@ const AdminAccess = () => {
 	const [openOldItemsInfo, setOpenOldItemsInfo] = useState(false);
 	const [openStoreData, setOpenStoreData] = useState(false);
 	const [userData, setUserData] = useState("");
-
+	const [openUserData, setOpenUserData] = useState(false);
 	const itemStats = () => {
 		createToken().then(token => {
 			axios
@@ -215,6 +215,31 @@ const AdminAccess = () => {
 								<button className="formBtn" onClick={storeStats}>
 									Update store stats{" "}
 								</button>
+							</div>
+						</Collapse>
+
+						<div
+							onClick={() => {
+								setOpenUserData(!openUserData);
+							}}
+							aria-controls="appData"
+							aria-expanded={openUserData}
+							className="appData__title"
+						>
+							<img
+								src={arrow}
+								className={`collapse-arrow ${
+									openUserData ? "collapse-arrow--open" : ""
+								}`}
+								alt=""
+							/>
+							SNAP & COMPARE User Data
+						</div>
+						<Collapse in={openUserData}>
+							<div id="appData" className="appData__stats">
+								<h4>Number of Users: {userData.ids}</h4>
+								<h4>Members: {userData.emails}</h4>
+								<h4>Guests: {userData.guests}</h4>
 							</div>
 						</Collapse>
 					</div>
