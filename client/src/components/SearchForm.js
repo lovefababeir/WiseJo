75c97fda3b3367da5b6d922fe2f764c152ccf49a
Love Fababeir <love.fababeir@hotmail.com>
2021-07-06 00:07:46 -0400

    Placed container around select input to border radius issue where it was still a rectangle in mobile view.

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -8,14 +8,17 @@ const SearchForm = ({ submitHandler, loading }) => {
 			<form className="form" action="submit" onSubmit={submitHandler}>
 				<div className="form__box">
 					<label className="form__label">Select a grocery store</label>
-					<select name="groceryStore" id="storeList" disabled={loading}>
-						<option value="Loblaws">Loblaws</option>
-						<option value="Longo's">Longo's</option>
-						<option value="No Frills">No Frills</option>
-						<option value="Sobeys">Sobeys</option>
-						<option value="Superstore">Superstore</option>
-						{/* <option value="Walmart">Walmart</option> */}
-					</select>
+
+					<div className="groceryStoreList">
+						<select name="groceryStore" id="storeList" disabled={loading}>
+							<option value="Loblaws">Loblaws</option>
+							<option value="Longo's">Longo's</option>
+							<option value="No Frills">No Frills</option>
+							<option value="Sobeys">Sobeys</option>
+							<option value="Superstore">Superstore</option>
+							{/* <option value="Walmart">Walmart</option> */}
+						</select>
+					</div>
 					<label className="form__label">Search an item</label>
 					<input
 						type="text"
